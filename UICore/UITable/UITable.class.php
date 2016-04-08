0ae2804884b271b9a2d63f12f58560a28aa4b949
Oliver Binns <mail@oliverbinns.co.uk>
2016-04-08 17:24:00 +0100

    Minor code reformatting on UITable class.

--- UICore/UITable/UITable.class.php
+++ UICore/UITable/UITable.class.php
@@ -5,15 +5,15 @@
 	 * Date: 01/03/2016
 	 * Time: 21:44
 	 */
-
+	
 	namespace phpHTML\UICore\UITable;
-
+	
 	use phpHTML\HTMLObject;
-
+	
 	/**
 	 * Class UITable
 	 *
-	 * @author Oliver Binns
+	 * @author  Oliver Binns
 	 * @package phpHTML\UICore\UITable
 	 */
 	class UITable extends HTMLObject{
@@ -21,26 +21,26 @@
 		 * @var UITableHeader $header The header object for this table.
 		 */
 		private $header;
-
+		
 		/**
 		 * @var UITableBody $body The body object for this table.
 		 */
 		private $body;
-
+		
 		/**
 		 * @var UITableFooter $footer The footer object for this table.
 		 */
 		private $footer;
-
+		
 		/**
 		 * UITable constructor.
 		 *
-		 * @param UITableHeader $header The header object for this table.
-		 * @param UITableBody $body The body object for this table.
-		 * @param UITableFooter $footer The footer object for this table.
-		 * @param array $classes Classes for use with CSS and Javascript
-		 * @param string $id HTML ID Attribute
-		 * @param string $on_click Javascript function to be run when the object is clicked
+		 * @param UITableHeader $header   The header object for this table.
+		 * @param UITableBody   $body     The body object for this table.
+		 * @param UITableFooter $footer   The footer object for this table.
+		 * @param array         $classes  Classes for use with CSS and Javascript
+		 * @param string        $id       HTML ID Attribute
+		 * @param string        $on_click Javascript function to be run when the object is clicked
 		 */
 		public function __construct($header = null, $body = null, $footer = null, $classes = [], $id = '', $on_click = ''){
 			parent::__construct($classes, $id, $on_click);
@@ -48,13 +48,13 @@
 			$this->body = $body;
 			$this->footer = $footer;
 		}
-
+		
 		/**
 		 * Returns the HTML string for this object
 		 * @return string HTML string
 		 */
 		public function __toString(){
-			$html = '<table ' . parent::__toString() . '>';
+			$html = '<table'.parent::__toString().'>';
 			$sections = [$this->header, $this->body, $this->footer];
 			foreach($sections as $section){
 				if(!empty($section)){
@@ -64,7 +64,7 @@
 			$html .= '</table>';
 			return $html;
 		}
-
+		
 		/**
 		 * Accessor
 		 * @return UITableHeader The header object for this table.
@@ -72,7 +72,7 @@
 		public function getHeader(){
 			return $this->header;
 		}
-
+		
 		/**
 		 * Accessor
 		 * @return UITableBody The body object for this table.
@@ -80,7 +80,7 @@
 		public function getBody(){
 			return $this->body;
 		}
-
+		
 		/**
 		 * Accessor
 		 * @return UITableFooter The footer object for this table.
@@ -88,25 +88,28 @@
 		public function getFooter(){
 			return $this->footer;
 		}
-
+		
 		/**
 		 * Mutator
+		 *
 		 * @param UITableHeader $header The header object for this table.
 		 */
 		public function setHeader($header){
 			$this->header = $header;
 		}
-
+		
 		/**
 		 * Mutator
+		 *
 		 * @param UITableBody $body The body object for this table.
 		 */
 		public function setBody($body){
 			$this->body = $body;
 		}
-
+		
 		/**
 		 * Mutator
+		 *
 		 * @param UITableFooter $footer The footer object for this table.
 		 */
 		public function setFooter($footer){
