76bf2c93106ef37d1bfc659394eeb0d6b3db8656
Oliver Binns <mail@oliverbinns.info>
2016-04-11 19:05:37 +0100

    Added new Section class for creating HTML <section> tags

--- UICore/UISection.class.php
+++ UICore/UISection.class.php
@@ -8,21 +8,21 @@
 	namespace phpHTML\UICore;
 
 	/**
-	 * Class UISpan
+	 * Class UISection
 	 *
 	 * @author Oliver Binns
 	 * @package phpHTML\UICore
 	 */
-	class UISpan extends UIView
+	class UISection extends UIView
 	{
 		/**
-		 * UISpan constructor.
+		 * UISection constructor.
 		 *
 		 * @param array|string $contents The content of this HTML division.
 		 * @param string       $id       HTML ID Attribute
 		 * @param array        $classes  Classes for use with CSS and Javascript
 		 */
 		public function __construct($contents = [], $classes = [], $id = ''){
-			parent::__construct('span', $contents, $classes, $id);
+			parent::__construct('section', $contents, $classes, $id);
 		}
 	}
